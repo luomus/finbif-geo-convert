719110a013d3dd3bea5890d53aa1efa5d90ac354
Will <willi@mmorris.email>
2021-06-07 16:15:20 +0300

    Add geo conversion script

---
+++ finbif_geo_convert.R
@@ -0,0 +1,200 @@
+library(finbif)
+library(sf)
+library(dplyr)
+library(tidyr)
+library(tools)
+library(rlang)
+
+bb <- function(x0, y0, x1, y1) {
+  ans <- c(x0, x0, x1, x1, x0, y0, y1, y1, y0, y0)
+  if (anyNA(ans)) return(NA)
+  ans <- matrix(ans, 5L)
+  list(st_polygon(list(ans)))
+}
+
+fmts <- c(
+  "bna", "csv", "geojson", "gml", "gpkg", "gxt", "jml", "nc", "ods", "shp",
+  "sqlite", "vdv", "xlsx"
+)
+
+short_geo_col_nms <- c(
+  "lonWGS84", "latWGS84", "lonEUREF", "latEUREF", "lon1KKJ",
+  "lat1KKJ", "lon10KKJ", "lat10KKJ", "lonMnWGS84", "latMnWGS84",
+  "lonMxWGS84", "latMxWGS84", "lonMnEUREF", "latMnEUREF",
+  "lonMxEUREF", "latMxEUREF", "lonMnKKJ", "latMnKKJ",
+  "lonMxKKJ", "latMxKKJ", "fprntWGS84"
+)
+
+geo_components <- list(
+  point_wgs84 = c("lon_wgs84", "lat_wgs84"),
+  point_euref = c("lon_euref", "lat_euref"),
+  point_1km_kkj = c("lon_1_kkj", "lat_1_kkj"),
+  point_10km_kkj = c("lon_10_kkj", "lat_10_kkj"),
+  bbox_wgs84 = c(
+    "lon_min_wgs84", "lat_min_wgs84", "lon_max_wgs84", "lat_max_wgs84"
+  ),
+  bbox_euref = c(
+    "lon_min_euref", "lat_min_euref", "lon_max_euref", "lat_max_euref"
+  ),
+  bbox_kkj = c("lon_min_kkj", "lat_min_kkj", "lon_max_kkj", "lat_max_kkj"),
+  polygon_wgs84 = "footprint_wgs84"
+)
+
+finbif_geo_convert <- function(
+  input, output, geo = "point", agg = NULL, crs = "wgs84", ...
+) {
+
+  fmt <- file_ext(output)
+
+  stopifnot("Format not supported" = fmt %in% fmts)
+
+  geo_col_names <- unlist(geo_components)
+
+  geo_cols <- switch(fmt, shp = short_geo_col_nms, geo_col_names)
+
+  names(geo_cols) <- geo_col_names
+
+  agg <- switch(geo, point = NULL, agg)
+
+  geo <- paste(c(geo, agg), collapse = "_")
+
+  geo_crs <- paste(c(geo, crs), collapse = "_")
+
+  geo_crs_is_avail <- geo_crs %in% names(geo_components)
+
+  geo_crs_avail <- geo_crs
+
+  if (!geo_crs_is_avail) {
+
+    geo_crs_avail <- switch(
+      geo,
+      bbox = "euref",
+      point = "euref",
+      point_1km = "kkj",
+      point_10km = "kkj",
+      polygon = "wgs84"
+    )
+
+    geo_crs_avail <- paste(geo, geo_crs_avail, sep = "_")
+
+  }
+
+  spatial_data <- fb_occurrence_load(
+    input, select = geo_components[[geo_crs_avail]], quiet = TRUE
+  )
+
+  spatial_data <- rowwise(spatial_data)
+
+  spatial_data <- switch(
+    geo_crs_avail,
+    point_wgs84 = mutate(
+      spatial_data, point_wgs84 = list(st_point(c(lon_wgs84, lat_wgs84)))
+    ),
+    point_euref = mutate(
+      spatial_data, point_euref = list(st_point(c(lon_euref, lat_euref)))
+    ),
+    point_1km_kkj = mutate(
+      spatial_data, point_1km_kkj = list(st_point(c(lon_1_kkj, lat_1_kkj)))
+    ),
+    point_10km_kkj = mutate(
+      spatial_data, point_10km_kkj = list(st_point(c(lon_10_kkj, lat_10_kkj)))
+    ),
+    bbox_wgs84 = mutate(
+      spatial_data,
+      bbox_wgs84 = bb(
+        lon_min_wgs84, lat_min_wgs84, lon_max_wgs84, lat_max_wgs84
+      )
+    ),
+    bbox_euref = mutate(
+      spatial_data,
+      bbox_euref = bb(
+        lon_min_euref, lat_min_euref, lon_max_euref, lat_max_euref
+      )
+    ),
+    bbox_kkj = mutate(
+      spatial_data,
+      bbox_kkj = bb(lon_min_kkj, lat_min_kkj, lon_max_kkj, lat_max_kkj)
+    ),
+    polygon_wgs84 = mutate(
+      spatial_data, footprint_wgs84 = replace_na(footprint_wgs84, "POINT EMPTY")
+    )
+  )
+
+  spatial_data <- ungroup(spatial_data)
+
+  spatial_data <- switch(
+    geo_crs_avail,
+    point_wgs84 = mutate(
+      spatial_data, point_wgs84 = st_as_sfc(point_wgs84, crs = st_crs(4326))
+    ),
+    point_euref = mutate(
+      spatial_data, point_euref = st_as_sfc(point_euref, crs = st_crs(3067))
+    ),
+    point_1km_kkj = mutate(
+      spatial_data, point_1km_kkj = st_as_sfc(point_1km_kkj, crs = st_crs(2393))
+    ),
+    point_10km_kkj = mutate(
+      spatial_data,
+      point_10km_kkj = st_as_sfc(point_10km_kkj, crs = st_crs(2393))
+    ),
+    bbox_wgs84 = mutate(
+      spatial_data, bbox_wgs84 = st_as_sfc(bbox_wgs84, crs = st_crs(4326))
+    ),
+    bbox_euref = mutate(
+      spatial_data, bbox_euref = st_as_sfc(bbox_euref, crs = st_crs(3067))
+    ),
+    bbox_kkj = mutate(
+      spatial_data, bbox_kkj = st_as_sfc(bbox_kkj, crs = st_crs(2393))
+    ),
+    polygon_wgs84 = mutate(
+      spatial_data, polygon_wgs84 = st_as_sfc(polygon_wgs84, crs = st_crs(4326))
+    )
+  )
+
+  spatial_data <- select(spatial_data, !any_of(geo_components[[geo_crs_avail]]))
+
+  if (!geo_crs_is_avail) {
+
+    crs <- switch(
+      as.character(crs), euref = 3067, kkj = 2393, wgs84 = 4326, crs
+    )
+
+    spatial_data[[geo_crs_avail]] <- st_transform(
+      spatial_data[[geo_crs_avail]], st_crs(crs)
+    )
+
+    names(spatial_data) <- geo_crs
+
+  }
+
+  st_geometry(spatial_data) <- geo_crs
+
+  data <- fb_occurrence_load(
+    input,
+    select = c(switch(fmt, shp = "short", "all"), paste0("-", geo_col_names))
+  )
+
+  data <- select(data, where(~any(!is.na(.x))))
+
+  if (identical(fmt, "shp")) {
+
+    data <- mutate(
+      data, across(where(is.character), ~substr(URLencode(.x), 1L, 254L))
+    )
+
+  }
+
+  data <- st_as_sf(
+    cbind(data, spatial_data),
+    agr = set_names(rep_len("identity", length(data)), names(data))
+  )
+
+  if (identical(fmt, "bna")) {
+
+    data <- filter(data, !st_is_empty(st_geometry(data)))
+
+  }
+
+  st_write(data, output, ...)
+
+}
