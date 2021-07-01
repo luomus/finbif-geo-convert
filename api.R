1e9ffd4e7d468f94c953bb9032a19913f926d01d
Will <willi@mmorris.email>
2021-07-01 16:51:53 +0300

    Fix timeout

--- api.R
+++ api.R
@@ -149,8 +149,8 @@ function(id, timeout = 30L, res) {
       zip <- character()
 
       sleep <- .2
-      timeout <- pmin(timeout, sleep)
-      timeout <- pmax(timeout, 60)
+      timeout <- pmax(timeout, sleep)
+      timeout <- pmin(timeout, 60)
 
       timer <- 0
 
