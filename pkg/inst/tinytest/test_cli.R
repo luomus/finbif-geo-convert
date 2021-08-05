9ac868a12bde400ac29327813df7819083f205e5
Will <willi@mmorris.email>
2021-08-05 15:10:50 +0300

    Add tests

---
+++ pkg/inst/tinytest/test_cli.R
@@ -0,0 +1,13 @@
+usage <- system2("convert", "-x", TRUE)
+
+expect_identical(
+  utils::head(usage), utils::head(system2("convert", "-h", TRUE))
+)
+
+expect_identical(
+  utils::tail(usage), utils::tail(utils::capture.output(show_formats()))
+)
+
+system("convert -o HBF.49381.shp HBF.49381.zip")
+
+expect_inherits(sf::st_read("HBF.49381.shp"), "sf")
