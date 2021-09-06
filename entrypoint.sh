6253194564d980dd664fcb1fc5b017de78644f99
Will <willi@mmorris.email>
2021-09-06 09:40:02 +0300

    Quote page title env var in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/RapiDoc/'$PAGE_TITLE'/g' \
+sed -i 's/RapiDoc/'"$PAGE_TITLE"'/g' \
   /usr/local/lib/R/site-library/rapidoc/dist/index.html
 echo "user:x:$(id -u):0::/home/user:/sbin/nologin" >> /etc/passwd
 "$@"
