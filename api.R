157ae51d89aaa54212d1db1ac9750fbe594908d0
wkmor1 <willi@mmorris.email>
2022-01-13 10:48:29 +0200

    Use id as file name when persisting errors

--- api.R
+++ api.R
@@ -186,7 +186,7 @@ function(
 
          writeLines(res[[1L]], err_file)
 
-         file.copy(err_file, "logs/errors", recursive = TRUE)
+         file.copy(err_file, sprintf("logs/errors/%s.txt", id))
 
       } else {
 
