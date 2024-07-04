ed1d24a42bd0dd4ce4865a492439d22896e0ae9f
wkmor1 <willi@mmorris.email>
2024-07-04 11:34:10 +0300

    Update process script

--- oc-process.sh
+++ oc-process.sh
@@ -5,11 +5,11 @@ f="template.yml"
 e=".env"
 
 while getopts ":f:e:i::" flag; do
-case $flag in
-f) f=${OPTARG} ;;
-e) e=${OPTARG} ;;
-i) i=${OPTARG} ;;
-esac
+  case $flag in
+    f) f=${OPTARG} ;;
+    e) e=${OPTARG} ;;
+    i) i=${OPTARG} ;;
+  esac
 done
 
 set -a
@@ -23,42 +23,46 @@ FINBIF_PRIVATE_API="unset"
 
 if [ "$BRANCH" != "main" ]; then
 
-HOST=$HOST_DEV
-FINBIF_PRIVATE_API="dev"
+  HOST=$HOST_DEV
+  MEMORY=$MEMORY_DEV
+  MAX_CHUNK_SIZE=$MAX_CHUNK_SIZE_DEV
+  FINBIF_PRIVATE_API="dev"
 
 fi
 
 if [ $i = "deploy" ]; then
 
-ITEM=".items[0]"
+  ITEM=".items[0]"
 
 elif [ $i = "service" ]; then
 
-ITEM=".items[1]"
+  ITEM=".items[1]"
 
 elif [ $i = "route" ]; then
 
-ITEM=".items[2]"
+  ITEM=".items[2]"
 
 elif [ $i = "all" ]; then
 
-ITEM=""
+  ITEM=""
 
 else
 
-echo "Object not found"
-exit 1
+  echo "Object not found"
+  exit 1
 
 fi
 
 echo "# $(oc project finbif-geo-convert)"
 
 oc process -f $f \
--p BRANCH=$BRANCH \
--p HOST=$HOST \
--p FINBIF_PRIVATE_API=$FINBIF_PRIVATE_API \
--p SMTP_SERVER=$SMTP_SERVER \
--p SMTP_PORT=$SMTP_PORT \
--p ERROR_EMAIL_TO=$ERROR_EMAIL_TO \
--p ERROR_EMAIL_FROM=$ERROR_EMAIL_FROM \
-| jq $ITEM
+  -p BRANCH=$BRANCH \
+  -p HOST=$HOST \
+  -p MEMORY=$MEMORY \
+  -p MAX_CHUNK_SIZE=$MAX_CHUNK_SIZE \
+  -p FINBIF_PRIVATE_API=$FINBIF_PRIVATE_API \
+  -p SMTP_SERVER=$SMTP_SERVER \
+  -p SMTP_PORT=$SMTP_PORT \
+  -p ERROR_EMAIL_TO=$ERROR_EMAIL_TO \
+  -p ERROR_EMAIL_FROM=$ERROR_EMAIL_FROM \
+  | jq $ITEM
