4cf78b6541e9246576b296eb914e78f6363641ff
wkmor1 <willi@mmorris.email>
2024-08-28 15:37:57 +0300

    Update test

--- inst/tinytest/test_pkg.R
+++ inst/tinytest/test_pkg.R
@@ -12,7 +12,7 @@ expect_inherits(finbif_geo_convert("laji-data2.tsv"), "sf")
 
 expect_inherits(finbif_geo_convert("laji-data2.tsv", geo = "footprint"), "sf")
 
-expect_inherits(finbif_geo_convert("HBF.6968.zip"), "sf")
+expect_inherits(finbif_geo_convert("HBF.6968.zip", n = 1), "sf")
 
 suppressWarnings(
   finbif_geo_convert(
